eb57e8581e36b5bc7ee6bf7ffa651fef5baaa1b0
abiha456 <abiha.amir456@gmail.com>
2022-05-30 14:15:46 -0700

    login with google

--- index.js
+++ index.js
@@ -1,123 +1,130 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const session = require('express-session')
-const passport = require('passport')
+const session = require("express-session");
+const passport = require("passport");
 
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 //Middleware
-app.use(session({
+app.use(
+  session({
     secret: "secret",
-    resave: false ,
-    saveUninitialized: true ,
-}))
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use(passport.initialize()) // init passport on every route call
-app.use(passport.session())    //allow passport to use "express-session"
-
+app.use(passport.initialize()); // init passport on every route call
+app.use(passport.session()); //allow passport to use "express-session"
 
 //Get the GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET from Google Developer Console
-const GOOGLE_CLIENT_ID = "883058441645-9u4tt0f9kn1apl6nb508tulh4ornqgmj.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET = "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"
+const GOOGLE_CLIENT_ID =
+  "883058441645-9u4tt0f9kn1apl6nb508tulh4ornqgmj.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
 
 authUser = (request, accessToken, refreshToken, profile, done) => {
-    return done(null, profile);
-  }
+  return done(null, profile);
+};
 
 //Use "GoogleStrategy" as the Authentication Strategy
-passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
-    passReqToCallback   : true
-  }, authUser));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    authUser
+  )
+);
 
+passport.serializeUser((user, done) => {
+  console.log(`\n--------> Serialize User:`);
+  console.log(user);
+  // The USER object is the "authenticated user" from the done() in authUser function.
+  // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.
 
-passport.serializeUser( (user, done) => { 
-    console.log(`\n--------> Serialize User:`)
-    console.log(user)
-     // The USER object is the "authenticated user" from the done() in authUser function.
-     // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.  
-
-    done(null, user)
-} )
-
+  done(null, user);
+});
 
 passport.deserializeUser((user, done) => {
-        console.log("\n--------- Deserialized User:")
-        console.log(user)
-        // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
-        // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
+  console.log("\n--------- Deserialized User:");
+  console.log(user);
+  // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
+  // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
 
-        done (null, user)
-}) 
-
+  done(null, user);
+});
 
 //Start the NODE JS server
-app.listen(3000, () => console.log(`Server started on port 3000...`))
-
+app.listen(3000, () => console.log(`Server started on port 3000...`));
 
 //console.log() values of "req.session" and "req.user" so we can see what is happening during Google Authentication
-let count = 1
+let count = 1;
 showlogs = (req, res, next) => {
-    console.log("\n==============================")
-    console.log(`------------>  ${count++}`)
+  console.log("\n==============================");
+  console.log(`------------>  ${count++}`);
 
-    console.log(`\n req.session.passport -------> `)
-    console.log(req.session.passport)
-  
-    console.log(`\n req.user -------> `) 
-    console.log(req.user) 
-  
-    console.log("\n Session and Cookie")
-    console.log(`req.session.id -------> ${req.session.id}`) 
-    console.log(`req.session.cookie -------> `) 
-    console.log(req.session.cookie) 
-  
-    console.log("===========================================\n")
+  console.log(`\n req.session.passport -------> `);
+  console.log(req.session.passport);
 
-    next()
-}
+  console.log(`\n req.user -------> `);
+  console.log(req.user);
 
-app.use(showlogs)
+  console.log("\n Session and Cookie");
+  console.log(`req.session.id -------> ${req.session.id}`);
+  console.log(`req.session.cookie -------> `);
+  console.log(req.session.cookie);
 
+  console.log("===========================================\n");
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope:
-      [ 'email', 'profile' ] }
-));
+  next();
+};
 
-app.get('/auth/google/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/dashboard',
-        failureRedirect: '/login'
-}));
+app.use(showlogs);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  })
+);
 
 //Define the Login Route
 app.get("/login", (req, res) => {
-    res.render("login.ejs")
-})
-
+  res.render("login.ejs");
+});
 
 //Use the req.isAuthenticated() function to check if user is Authenticated
 checkAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) { return next() }
-  res.redirect("/login") 
-}
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+};
 
 //Define the Protected Route, by using the "checkAuthenticated" function defined above as middleware
 app.get("/dashboard", checkAuthenticated, (req, res) => {
-  res.render("dashboard.ejs", {name: req.user.displayName})
-})
+  res.render("dashboard.ejs", { name: req.user.displayName, user: req.user });
+});
 
 //Define the Logout
-app.post("/logout", (req,res) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect("/login")
-      })
-    // req.logOut()
-    
-    console.log(`-------> User Logged out`)
-})
+app.post("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+  // req.logOut()
+
+  console.log(`-------> User Logged out`);
+});
